6ea0a8b6dda0f03e9be4df208545a6109a913114
harshavardhan1235 <167059656+harshavardhan1235@users.noreply.github.com>
2024-04-20 21:25:59 +0530

    Update Boyer_more.py

--- Pyhton_Impliemtation/Boyer_more.py
+++ Pyhton_Impliemtation/Boyer_more.py
@@ -1,31 +1,45 @@
 NO_OF_CHARS = 256
 
-# A utility function to get maximum of two integers
-def max(a, b):
-    return a if a > b else b
-
-# The preprocessing function for Boyer Moore's bad character heuristic
-def badCharHeuristic(string, size):
+def badCharHeuristic(string):
     badchar = [-1] * NO_OF_CHARS
-    for i in range(size):
-        badchar[ord(string[i])] = i
+    for i, char in enumerate(string):
+        badchar[ord(char)] = i
     return badchar
 
-def search(txt, pat):
-    m = len(pat)
-    n = len(txt)
-    badchar = badCharHeuristic(pat, m)
-    s = 0  # s is shift of the pattern with respect to text
+def preProcessGoodSuffix(pattern):
+    m = len(pattern)
+    suff = [-1] * m
+    j = m - 1
+    for i in range(m - 2, -1, -1):
+        while j >= 0 and pattern[j] != pattern[i]:
+            j = m - 1 - (m - 1 - j)
+        j -= 1
+        suff[i] = j + 1
+    return suff
+
+def search(text, pattern):
+    m = len(pattern)
+    n = len(text)
+    badchar = badCharHeuristic(pattern)
+    suff = preProcessGoodSuffix(pattern)
+
+    s = 0
     while s <= (n - m):
         j = m - 1
-        while j >= 0 and pat[j] == txt[s + j]:
+        while j >= 0 and pattern[j] == text[s + j]:
             j -= 1
-        if j < 0:
-            print("\n pattern occurs at shift =", s)
-            s += m - badchar[ord(txt[s + m])] if (s + m < n) else 1
-        else:
-            s += max(1, j - badchar[ord(txt[s + j])])
 
-txt = input("Enter the text: ")
-pat = input("Enter the pattern to search: ")
-search(txt, pat)
+        if j < 0:
+            print("Pattern occurs at shift =", s)
+            s += m - badchar[ord(text[s + m])] if (s + m < n) else 1
+        else:
+            s += max(1, max(j - badchar[ord(text[s + j])], j - suff[j]))
+
+def main():
+    with open("input.txt", "r") as file:
+        text, pattern = file.readline().strip(), file.readline().strip()
+
+    search(text, pattern)
+
+if __name__ == "__main__":
+    main()
